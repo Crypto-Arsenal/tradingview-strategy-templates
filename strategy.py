df1ec5ccbc4c56afb925bafa3e6a0034508da8a2
Alan Wu <ac2wu@edu.uwaterloo.ca>
2022-09-28 22:23:34 -0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -7,7 +7,7 @@ class Strategy(StrategyBase):
         exchange, pair, base, quote = CA.get_exchange_pair()
         quote_balance = CA.get_balance(exchange, quote)
         self.init_available_quote_amount = quote_balance.available
-        CA.log('total inital' + str(quote) + ' quote amount: ' + str(self.init_available_quote_amount))
+        CA.log('total inital ' + str(quote) + ' quote amount: ' + str(self.init_available_quote_amount))
 
     def on_tradingview_signal(self, signal, candles):
         CA.log('on_tradingview_signal: ' + str(signal))
@@ -23,13 +23,13 @@ class Strategy(StrategyBase):
             quote_balance = CA.get_balance(exchange, quote)
             available_quote_amount = quote_balance.available
             # if profit then stick with init amount / if lose money then use available_quote_amount
-            order_quote_amount = min(self.init_available_quote_amount, available_quote_amount)
+            order_quote_amount = min(self.init_available_quote_amount, available_quote_amount)  * self.percentage_of_initial_asset
             order_price =   float(signal.get('limit')) if signal.get('limit') is not None else candles[exchange][pair][0]['close']
            
-            CA.log("order_quote_amount " + str(order_quote_amount))
-            CA.log("order_price " + str(order_price))
+            CA.log("USDT To Place Order: " + str(order_quote_amount))
+            CA.log("Order Price: " + str(order_price))
 
-            fixed_amount = (order_quote_amount * self.percentage_of_initial_asset) / order_price
+            fixed_amount = (order_quote_amount) / order_price
             
             CA.log("Order Amount: " + str(fixed_amount))
             CA.place_order(exchange, pair, action, signal.get('limit'), fixed_amount, signal.get('percent'), signal.get('clientOrderId'), signal.get('profit'), signal.get('loss'))
