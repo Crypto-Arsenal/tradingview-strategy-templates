53a401a3b67f3dc79965f4190b75bc48b3f22192
Alan Wu <ac2wu@edu.uwaterloo.ca>
2022-11-25 14:07:52 +0800

    Update sync_position_side.py

--- sync_position_side.py
+++ sync_position_side.py
@@ -3,8 +3,6 @@ class Strategy(StrategyBase):
         self.period = 60
         self.subscribed_books = {}
         self.options = {}
-        self.curTotalPositionSize = None
-        self.ORDER_PORTION = 1
 
     def on_tradingview_signal(self, signal, candles):
         exchange, pair, base, quote = CA.get_exchange_pair()
@@ -14,124 +12,58 @@ class Strategy(StrategyBase):
         """
         self.newPositionSize = None
 
-        signal_action = signal.get('action')
         log = signal.get('log')
 
         CA.log('📩 TradingView log: ' + str(log))
 
-        items = log.split("/") # comment/market_position/market_position_size
+        items = log.split("/")  # comment/market_position/market_position_size
 
-        if items and len(items) >= 3:
-            self.prevPositionSide = items[2]
-            self.newPositionSide = items[1] # market_position: long, short, flat
-
-        # if self.newPositionSize is None:
-        #     CA.log("failed to parse position size from log " + log)
-        #     return
-
-        if self.newPositionSide is None:
-            CA.log("⛔ failed to parse position from log " + log)
-            return
-
-        # self.newPositionSize = float(self.newPositionSize.strip()) * self.ORDER_PORTION
-        # self.prevPositionSize = float(self.prevPositionSize.strip()) * self.ORDER_PORTION
-
-        if self.newPositionSide ==  "long":
-            self.newPositionSize = 1
-        elif self.newPositionSide == "short":
-            self.newPositionSize =  -1
-        elif self.newPositionSide == "flat":
-            self.newPositionSize = 0 # not sure
+        if items and len(items) >= 2:
+            self.prevTVPositionSide = items[2]
+            self.newTVPositionSide = items[1]  # market_position: long, short, flat
         else:
-            CA.log("⛔ failed to parse position from log: expect long, short, or flat " + log)
+            CA.log('⛔ Invalid signal')
             return
 
-        if self.prevPositionSide ==  "long":
-            self.prevPositionSize = 1
-        elif self.prevPositionSide == "short":
-            self.prevPositionSize =  -1
-        elif self.prevPositionSide == "flat":
-            self.prevPositionSize = 0 # not sure
+        self.prevCAPositionSize, self.curCAPositionSide = self.get_total_position_size_and_side()
+
+        # Already in sync
+        if self.newTVPositionSide == self.curCAPositionSide:
+            CA.log('⚠️ Position already synced')
+            return
+
+        # Entry Long
+        if self.newTVPositionSide == "long":
+            if self.curCAPositionSide == "short": # long -> short
+                CA.place_order(exchange, pair, action='close_short', conditional_order_type='OTO', percent=100,
+                                   child_conditional_orders=[{'action': 'open_long',  'percent': 100}])
+            elif self.curCAPositionSide == "flat":  # long -> flat
+                CA.place_order(exchange, pair, action='open_long', percent=100)
+        # Entry Short
+        elif self.newTVPositionSide == "short":
+            if self.curCAPositionSide == "long": # short -> long
+                    CA.place_order(exchange, pair, action='close_long', conditional_order_type='OTO', percent=100,
+                                   child_conditional_orders=[{'action': 'open_short',  'percent': 100}])
+            elif self.curCAPositionSide == "flat":  # short -> flat
+                CA.place_order(exchange, pair, action='open_short', percent=100)
+        # No position
+        elif self.newTVPositionSide == "flat":
+            if self.curCAPositionSide == "short":
+                CA.place_order(exchange, pair, action='close_short', percent=100)
+            elif self.curCAPositionSide == "long":
+                CA.place_order(exchange, pair, action='close_long', percent=100)
         else:
-            CA.log("⛔ failed to parse position from log: expect long, short, or flat " + log)
+            CA.log("⛔ No action")
             return
 
-        """
-        Set Current Position
-        """
-
-        # will be current position
-        self.curTotalPositionSize, self.curActualPositionSize = self.get_total_position_size_and_side()
-
-        if self.curTotalPositionSize is None:
-            CA.log("⛔ cannot get current total position")
-            return
-
-        # will be current position
-        CA.log("💬 current Total Position " + str(self.curActualPositionSize))
-
-        # start bot only if we were at 0 signal
-        if self.curTotalPositionSize == 0 and self.prevPositionSize != 0:
-            CA.log("⚠️ Prev Position is not 0; will wait and start position once we are at 0")
-            return
-
-
-        # 100%
-        if self.curTotalPositionSize == self.newPositionSize:
-            CA.log("⚠️ Position already synced")
-            return
-
-        if self.curTotalPositionSize > self.newPositionSize:
-            if self.newPositionSize >= 0:
-                # 3 -> 2
-                # amount = self.curTotalPositionSize - self.newPositionSize
-                CA.log("💬 Amount to close long: " + str(self.curActualPositionSize))
-                CA.place_order(action="close_long", exchange=exchange, pair=pair,  percent=100)
-            else:
-                # 2 -> -1
-                if self.curTotalPositionSize > 0:
-                    # "closeLong/openShort"
-                    # close_long_amount = self.curTotalPositionSize
-                    # open_short_amount = abs(self.newPositionSize)
-                    CA.log("💬 Amount to close long: " + str(self.curActualPositionSize))
-                    CA.log("💬 Amount to open short: 100%")
-                    CA.place_order(exchange, pair, action='close_long', percent=100, conditional_order_type='OTO', child_conditional_orders=[{
-                        'action': 'open_short', 'percent': 100
-                    }])
-                else:
-                    # -3 -> -2 = 1
-                    # amount = abs(self.newPositionSize - self.curTotalPositionSize)
-                    CA.log("💬 Amount to open short: " + str(self.curActualPositionSize))
-                    CA.place_order(action="open_short", exchange=exchange, pair=pair,  percent=100)
-        else:
-            if self.newPositionSize <= 0:
-                #  -3 -> -1
-                # amount = abs(self.curTotalPositionSize - self.newPositionSize)
-                CA.log("💬 Amount to close short: " + str(self.curActualPositionSize))
-                CA.place_order(action="close_short", exchange=exchange, pair=pair,  percent=100)
-            else:
-                if self.curTotalPositionSize >= 0:
-                    # 1 -> 2
-                    # amount = (self.newPositionSize - self.curTotalPositionSize )
-                    CA.log("💬 Amount to open long: " + str(self.curActualPositionSize))
-                    CA.place_order(action="open_long", exchange=exchange, pair=pair,  percent=100)
-                else:
-                    # close_short_amount = self.curTotalPositionSize
-                    # open_long_amount = abs(self.newPositionSize) 
-                    CA.log("💬 Amount to close short: " + str(self.curActualPositionSize))
-                    CA.log("💬 Amount to open long:  100%" )
-                    CA.place_order(exchange, pair, action='close_short',  percent= 100, conditional_order_type='OTO', child_conditional_orders=[{
-                        'action': 'open_long',  'percent': 100
-                    }])
-
+    def trade(self, candles):
+        pass
+    
     def on_order_state_change(self,  order):
         if order.status == CA.OrderStatus.FILLED:
             CA.log('🎉 LATEST POS: ' + str(self.get_total_position_size_and_side()[0]))
 
-    def trade(self, candles):
-        pass
-
-    # return current total position: -n 0, +n  where n is number of contracts
+        # return current total position: -n 0, +n  where n is number of contracts
     def get_total_position_size_and_side(self):
         exchange, pair, base, quote = CA.get_exchange_pair()
 
@@ -148,12 +80,12 @@ class Strategy(StrategyBase):
             total_short_position_size = short_position.total_size
 
         if total_long_position_size is None and total_short_position_size is None:
-            curTotalPositionSize = (0,0)
+            curTotalPositionSize = (0, 'flat')
 
         if total_long_position_size is not None:
-            curTotalPositionSize = (abs(total_long_position_size),1)
+            curTotalPositionSize = (abs(total_long_position_size), 'long')
 
         if total_short_position_size is not None:
-            curTotalPositionSize = (-1 * abs(total_short_position_size),-1)
+            curTotalPositionSize = (-1 * abs(total_short_position_size), 'short')
 
         return curTotalPositionSize
