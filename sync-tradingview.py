4f7efbce4291c219283734f4ed196ec828c9195a
Alan Wu <ac2wu@edu.uwaterloo.ca>
2022-12-22 05:12:11 +0800

    Update sync-tradingview.py

--- sync-tradingview.py
+++ sync-tradingview.py
@@ -5,9 +5,9 @@ class Strategy(StrategyBase):
         self.options = {}
         exchange, pair, base, quote = CA.get_exchange_pair()
         quote_balance = CA.get_balance(exchange, quote)
-        # self.initial_capital = quote_balance.available
         self.ca_initial_capital = quote_balance.available
-        CA.log('Total inital ' + str(quote) + ' quote amount: ' + str(self.ca_initial_capital))
+        self.ca_total_capital = quote_balance.available
+        CA.log('Total inital ' + str(quote) + ' quote amount: ' + str(self.ca_total_capital))
 
     def on_tradingview_signal(self, signal, candles):
         exchange, pair, base, quote = CA.get_exchange_pair()
@@ -26,21 +26,21 @@ class Strategy(StrategyBase):
                 "price": {{strategy.order.price}},
             }
         ===   
-        {
-            "connectorName":"name",
-            "connectorToken":"token",
-            "log": "{{strategy.order.comment}}", 
-            "entryOrder": {
-                "value": 100,
-                "mode": "availableBalancePercent",
-            },
-            "position": {
-                "position": "{{strategy.market_position}}", 
-                "position_size": {{strategy.market_position_size}},
-                "prev_position": "{{strategy.prev_market_position}}",
-                "prev_position_size": {{strategy.prev_market_position_size}}
-            }
-        }
+{
+   "connectorName":"REPLACE_NAME",
+   "connectorToken":"REPLACE_TOKEN",
+   "log":"short",
+   "entryOrder":{
+      "value":100,
+      "mode":"availableBalancePercent"
+   },
+   "position":{
+      "side":"{{strategy.market_position}}",
+      "size":{{strategy.market_position_size}},
+      "prev_side":"{{strategy.prev_market_position}}",
+      "prev_size":{{strategy.prev_market_position_size}}
+   }
+}
         """
 
         position = signal.get('position')
@@ -58,43 +58,59 @@ class Strategy(StrategyBase):
 
 
         # 檢查訊號正確性
-        if tv_order_mode is None or tv_position is None or tv_prev_position is None:
+        if tv_order_mode is None or tv_position is None:
             return CA.log('⛔ Invalid signal')
 
         ca_position = self.get_ca_position()
         quote_balance = CA.get_balance(exchange, quote)
         ca_available_capital = quote_balance.available
 
+        # 如果不能給之前的倉位那就 預設至 ca_position
+        if tv_prev_position is None:
+            tv_prev_position = ca_position
+
 
         """
-         - 如果反向開單或是加倉
-         - 用 tv_capital 算出 要開 compound_capital 的幾 % 
+        如果反向開單或是加倉
         """
         if (abs(tv_position) > abs(tv_prev_position) and tv_position * tv_prev_position >= 0) or tv_position * tv_prev_position < 0:
             ca_order_captial = ca_available_capital
+            # PPC  複利
             if tv_order_mode == "availableBalancePercent":
                 ca_order_captial = None
                 tv_order_percent_of_capitial = tv_order_value
+            # 單利
+            elif tv_order_mode == "fixedBalancePercent":
+                ca_order_captial = None
+                diff = ca_available_capital - self.ca_initial_capital
+                tv_order_percent_of_capitial = tv_order_value
+                # 賺錢
+                if diff > 0:
+                    # 算多賺的是幾％
+                    offset_percent = (ca_available_capital / diff) * 100
+                    tv_order_percent_of_capitial = tv_order_value - offset_percent
+            # PPC  複利 加倉
             elif tv_order_mode == "totalBalancePercent":
-                ca_order_captial = self.ca_initial_capital
+                ca_order_captial = self.ca_total_capital
                 tv_order_percent_of_capitial = tv_order_value / 100
             elif tv_order_mode == "fixedTotalBalance":
+                ca_order_captial = self.ca_total_capital
                 # close short -> open long (一個正 一個反) 有一些order數量是反轉時要關艙的 所以要拿掉
                 if tv_position * tv_prev_position < 0: # 代表倉位方向不一樣
                     tv_order_size = tv_order_size - abs(tv_prev_position) # 其實就是 tv_position
                 # 用下單金額和權益去反推TV下單% tv_order_value 是我們的固定本金
                 tv_order_percent_of_capitial = (tv_order_size * tv_order_price) / tv_order_value
             else:
-                CA.log("Invalid tv_order_mode" + str(tv_order_mode))
+                CA.log("⛔ Invalid tv_order_mode" + str(tv_order_mode))
             
             if  ca_order_captial is None:  # availableBalancePercent
                 newOrderAmount = dict(percent=tv_order_percent_of_capitial * CA.get_leverage())   # default to 1
-                CA.log("CA開倉比例% " + str(tv_order_percent_of_capitial) + " \n CA下單金額%" + str(tv_order_percent_of_capitial) +  " \n CA入場本金$: " + str(self.ca_initial_capital)  + " \n CA可用資金$: " + str(ca_available_capital))
+                CA.log("CA開倉比例% " + str(tv_order_percent_of_capitial) + " \n CA下單金額%" + str(tv_order_percent_of_capitial) +  " \n CA入場本金$: " + str(self.ca_total_capital)  + " \n CA可用資金$: " + str(ca_available_capital))
             else:
                 # 用CA空倉時的金額去下開或加倉的金額
                 notional = ca_order_captial * tv_order_percent_of_capitial * CA.get_leverage() # default to 1
                 newOrderAmount = dict(notional = notional)   
-                CA.log("CA開倉比例% " + str(tv_order_percent_of_capitial * 100) + " \n CA下單金額$ " + str(notional) +  " \n CA入場本金$: " + str(self.ca_initial_capital)  + " \n CA可用資金$: " + str(ca_available_capital))
+                CA.log("CA開倉比例% " + str(tv_order_percent_of_capitial * 100) + " \n CA下單金額$ " + str(notional) +  " \n CA入場本金$: " + str(self.ca_total_capital)  + " \n CA可用資金$: " + str(ca_available_capital))
 
             # close short -> open long 不用管 prev_tv_position 因為我們知道一定會開多 但是要先確保 CA 倉位是對的
             if tv_position > 0 and ca_position < 0:
@@ -117,6 +133,18 @@ class Strategy(StrategyBase):
             if ca_position == 0:
                 return CA.log("沒有倉位不用關")
 
+            # flat 全關
+            if tv_position == 0:
+                action = "close_long" if ca_position > 0 else "close_short"
+                return CA.place_order(exchange, pair, action=action, percent=100)
+            elif tv_position > 0 and ca_position < 0:
+                CA.log("倉位錯亂 全關空倉")
+                return CA.place_order(exchange, pair, action="close_short", percent=100)
+            elif tv_position < 0 and ca_position > 0:
+                CA.log("倉位錯亂 全關多倉")
+                return CA.place_order(exchange, pair, action="close_long" , percent=100)
+
+
             # 用TV前和後倉位去看關了多少 不行超過 1
             tv_order_percent_of_position = min((tv_prev_position - tv_position) / tv_prev_position, 1) * 100
             
@@ -136,13 +164,13 @@ class Strategy(StrategyBase):
 
         if order.status == CA.OrderStatus.FILLED:
             # 看CA的倉位已經用了多少%的本金去開了
-            ca_position_percent_of_capital = (self.ca_initial_capital - ca_available_capital) / self.ca_initial_capital
-            CA.log("🎉 現在CA倉位數量: " + str(ca_position) + " 本金%: " + str(ca_position_percent_of_capital * 100) + " \n CA入場本金$: " + str(self.ca_initial_capital)  + " \n CA可用資金$: " + str(ca_available_capital))
+            ca_position_percent_of_capital = (self.ca_total_capital - ca_available_capital) / self.ca_total_capital
+            CA.log("🎉 現在CA倉位數量: " + str(ca_position) + " 本金%: " + str(ca_position_percent_of_capital * 100) + " \n CA入場本金$: " + str(self.ca_total_capital)  + " \n CA可用資金$: " + str(ca_available_capital))
             
       # 平倉時 設置新的開倉金
         if ca_position == 0:
-            self.ca_initial_capital = ca_available_capital
-            CA.log('新的CA開倉本金: ' + str(self.ca_initial_capital))
+            self.ca_total_capital = ca_available_capital
+            CA.log('新的CA開倉本金: ' + str(self.ca_total_capital))
             
     def get_position_from_size_and_side(self, positionSize, positionSide):
         if positionSide is None or positionSize is None:
